29bcce3728d3c1480c7e5bbcf4a279b6a404544f
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-10-26 09:55:32 -0700

    Add backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "inf-tfstate-us-gov-west-1-229685449397"
-    key            = "terraform-state-files/terraform.tfstate"
-    region         = "us-gov-west-1"
-    dynamodb_table = "tf_remote_state"
-  }
-}
