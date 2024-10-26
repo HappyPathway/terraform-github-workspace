6a4d5fa5bb9373a4d8df455fb50f3b6103c7364f
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-10-26 12:26:02 -0700

    Update main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -10,28 +10,9 @@ module "github_actions" {
         enforce_reviewers = true
         teams             = ["terrform-reviewers"]
       }
-      #   state_config = {
-      #     bucket         = "my-terraform-state-bucket"
-      #     key_prefix     = "terraform/production/terraform-github-workspace"
-      #     region         = "us-west-2"
-      #     dynamodb_table = "terraform-lock-table"
-      #     set_backend    = true
-      #   }
       deployment_branch_policy = {
         restrict_branches = false
       }
-      secrets = [
-        {
-          name  = "SECRET_KEY"
-          value = "stagingsecretvalue"
-        }
-      ]
-      vars = [
-        {
-          name  = "ENV_VAR"
-          value = "staging"
-        }
-      ]
     }
   ]
 }
