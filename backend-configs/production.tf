87bbf5924868c7d87eba770f83b63b79cb266506
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-10-26 09:24:10 -0700

    Add backend-configs/production.tf

--- backend-configs/production.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "terraform/production/terraform-github-workspace/production.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-lock-table"
-  }
-}
